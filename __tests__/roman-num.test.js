1baeb8ff4a5e3237e7f07f32232a5bce3ff7ea54
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-02 14:09:08 -0800

    add red state test for toRoman(3) = 'III' Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com> Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>

--- __tests__/roman-num.test.js
+++ __tests__/roman-num.test.js
@@ -9,4 +9,8 @@ describe('toRoman', () => {
   test("should correctly convert '2' to 'II'", () => {
     expect(toRoman(2)).toEqual('II');
   });
+
+  test("should correctly convert '3' to 'III'", () => {
+    expect(toRoman(3)).toEqual('III');
+  });
 });
