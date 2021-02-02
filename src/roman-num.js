50dbc85a7f1b4c5f7e19c8aeb20cc356f006ed8e
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-02 14:19:50 -0800

    pass green state test for toRoman(3) = 'III'
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>

--- src/roman-num.js
+++ src/roman-num.js
@@ -1,9 +1,8 @@
 export default function toRoman(num) {
-  if (num === 3) {
-    return "III";
-  } else if (num === 2) {
-    return "II";
-  } else if (num === 1) {
-    return "I";
+  let romanNum = "";
+  while (num > 0) {
+    romanNum += "I";
+    num --;
   }
+  return romanNum;
 }
