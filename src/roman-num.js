94ebd4ba6e2d81e2b574225ad0532604393b936a
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-02 14:39:51 -0800

    pass green stage test for toRoman(4) = 'IV'
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>

--- src/roman-num.js
+++ src/roman-num.js
@@ -1,8 +1,18 @@
+// dictionary: M=1000, CM=900, D=500, CD=400, C=100, XC=90, L=50, XL=40, X=10, IX=9, V=5, IV=4, I=1
+
+const numerals = [
+  {value: 4, numeral: "IV"},
+  {value: 1, numeral: "I"}
+]
+
 export default function toRoman(num) {
   let romanNum = "";
-  while (num > 0) {
-    romanNum += "I";
-    num --;
-  }
+
+  numerals.forEach(function(item) {
+    while (num >= item.value) {
+      romanNum += item.numeral;
+      num -= item.value;
+    }
+  });
   return romanNum;
 }
